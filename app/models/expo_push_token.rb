7ac705f324d1b73243354d022fe9fe9562120d6d
Thibault <933kss@gmail.com>
2020-12-08 22:12:05 +0200

    remove tokens from other experiences

--- app/models/expo_push_token.rb
+++ app/models/expo_push_token.rb
@@ -3,5 +3,13 @@ class ExpoPushToken < ActiveRecord::Base
 
   validates :token, presence: true, uniqueness: { scope: :user }
   validates :user,  presence: true
-end
+  validates :experience_id, presence: true
 
+  after_save :remove_other_experiences
+
+  private
+
+  def remove_other_experiences
+    ExpoPushToken.where('user=? AND experience_id!=?', self.user, self.experience_id).delete_all
+  end
+end
